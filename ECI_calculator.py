76aaab044ee9e477acb95ae30478178aa842d2f8
Marvin Schwander <mschwander@student.ethz.ch>
2025-11-10 13:20:49 +0100

    Created a mapping of ECI values for the whole BACI dataset and the Energy subset

--- ECI_calculator.py
+++ ECI_calculator.py
@@ -6,7 +6,6 @@ import os
 df = pd.read_csv("Data/baci_energy_subset.csv")
 #df = pd.read_csv(r"C:\Users\marvi\OneDrive\Semester Thesis\BACI_HS22_V202501\BACI_HS22_Y2023_V202501.csv")
 
-
 # Run ecomplexity with column mapping
 eci_results = ecomplexity(
     df,
@@ -18,27 +17,9 @@ eci_results = ecomplexity(
     }
 )
 
-# Define path to your GitHub data folder
-save_path = os.path.join("Data", "eci_results.pkl")
-
-# Save the whole results object
-with open(save_path, "wb") as f:
-    pickle.dump(eci_results, f)
-
-print(f"eci_results saved to {save_path}")
+# Save to CSV
+eci_results.to_csv("Results/eci_results_Energy.csv", index=False)
+print("Saved eci_results to Results/eci_results_Energy.csv")
 
 print(eci_results.head())
 print(eci_results.columns)
-
-'''
-# Country-level complexity (ECI)
-eci_df = eci_results['complexity_country']
-print(eci_df.head())
-
-# Product-level complexity (PCI)
-pci_df = eci_results['complexity_product']
-print(pci_df.head())
-
-eci_2023 = eci_df[eci_df["t"] == 2023].sort_values("eci", ascending=False)
-print(eci_2023.head(10))
-'''
